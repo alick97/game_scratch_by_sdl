a36f674913c441618d2c35f23a36ba5df50028f8
alick <alick97@outlook.com>
2018-12-10 01:34:06 +0800

    sepatate the functions into their own class

---
+++ src/Game.h
@@ -0,0 +1,27 @@
+#ifndef __Game__
+#define __Game__
+#include <SDL.h>
+#include <iostream>
+
+class Game {
+public:
+    Game() {}
+    ~Game() {}
+
+    // simple set the running variable to true
+    bool init(const char* title, int xpos, int ypos, int width,
+    int height, int flags);
+    void render();
+    void update();
+    void handleEvents();
+    void clean();
+
+    // a function to access the private running variable
+    bool running() { return m_bRunning; }
+
+private:
+    SDL_Window* m_pWindow;
+    SDL_Renderer* m_pRenderer;
+    bool m_bRunning;
+};
+#endif /* defined(__Game__) */
